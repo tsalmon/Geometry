0754be5effb2b0ea07b5c16e5fffb9c5b6161256
Thomas Salmon <th_s@hotmail.fr>
2012-11-29 19:52:18 +0100

    carpet : last touch

--- Salmon_carpet.py
+++ Salmon_carpet.py
@@ -1,6 +1,6 @@
 from Tkinter import *
 
-def draw(draw, d, n, i):
+def Draw(draw, d, n, i):
 	if( i > 0):
 		aux = (d+n)/2
 		drawing.create_rectangle(d, d, n, n, outline="white", fill="white")
@@ -16,13 +16,13 @@ def draw(draw, d, n, i):
 		#down left 
 		points = [d, aux, aux, aux, d, n, aux, n]
 		drawing.create_polygon(points)
-		draw(drawing, (d+500/2)/2, (500/2+n)/2,i-1) 
+		Draw(drawing, (d+500/2)/2, (500/2+n)/2,i-1) 
 
 if __name__ == '__main__':
 	Root = Tk()
 	drawing = Canvas(Root, width = 500, height = 500)
 	drawing.pack()
-	draw(drawing, 0, 500, 5)
+	Draw(drawing, 0, 500, 5)
 	Root.title("Salmon carpet")
 	Root.mainloop()
 
