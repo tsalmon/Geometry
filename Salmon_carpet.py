66a232f08b375a59c3f4f96a1e5ff23243c96c10
Thomas Salmon <th_s@hotmail.fr>
2012-11-29 19:50:51 +0100

    carpet : last touch

--- Salmon_carpet.py
+++ Salmon_carpet.py
@@ -1,31 +1,28 @@
 from Tkinter import *
 
-def dessiner(dessin, d, n, i):
+def draw(draw, d, n, i):
 	if( i > 0):
 		aux = (d+n)/2
-		dessin.create_rectangle(d, d, n, n, outline="white", fill="white")
-		# Haut gauche
+		drawing.create_rectangle(d, d, n, n, outline="white", fill="white")
+		# top left
 		points = [d, d, d, aux, aux, d, aux, aux]
-		dessin.create_polygon(points)
-		#Bas droite
+		drawing.create_polygon(points)
+		#down right
 		points = [aux, aux, aux, n, n, aux, n, n]
-		dessin.create_polygon(points)
-		#Haut droite 
+		drawing.create_polygon(points)
+		#up right 
 		points = [aux, d, n, d, aux, aux, n, aux]
-		dessin.create_polygon(points)
-		#Bas gauche 
+		drawing.create_polygon(points)
+		#down left 
 		points = [d, aux, aux, aux, d, n, aux, n]
-		dessin.create_polygon(points)
-		print("d: "+str(d)+", "+"n: "+str(n))
-		dessiner(dessin, (d+500/2)/2, (500/2+n)/2,i-1) 
+		drawing.create_polygon(points)
+		draw(drawing, (d+500/2)/2, (500/2+n)/2,i-1) 
 
 if __name__ == '__main__':
-	Fenetre = Tk()
-	dessin = Canvas(Fenetre, width = 500, height = 500)
-	dessin.create_line(0,250,500,250)
-	dessin.create_line(250,0,250,500)
-	dessin.pack()
-	dessiner(dessin, 0, 500, 5)
-	Fenetre.title("Salmon carpet")
-	Fenetre.mainloop()
+	Root = Tk()
+	drawing = Canvas(Root, width = 500, height = 500)
+	drawing.pack()
+	draw(drawing, 0, 500, 5)
+	Root.title("Salmon carpet")
+	Root.mainloop()
 
